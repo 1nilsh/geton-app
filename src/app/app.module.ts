b5f0ce2ebf543a44c850c7cd6bcce623c23290d7
Nils Hodys <dev@pmail.nils.bz>
2021-04-12 17:58:31 +0200

    [master] pwa offline support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthenticationService } from './applicationlogic/auth/services/authentication.service';
 import { JwtInterceptorService } from './applicationlogic/auth/services/jwt-interceptor.service';
 import { SyncService } from './applicationlogic/sync/services/sync.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -26,6 +28,12 @@ import { SyncService } from './applicationlogic/sync/services/sync.service';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
