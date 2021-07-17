13336a223e507f87ae8728ead9d8ef54c92cf257
Nils Hodys <dev@pmail.nils.bz>
2021-07-17 20:31:50 +0200

    [master] remove unused import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthenticationGuard } from './applicationlogic/auth/guard/authentication.guard';
-import { ImprintPage } from './presentation/pages/imprint/imprint.page';
 
 const routes: Routes = [
   {
